c91d30f6fdeec62ea60dec819ce2de54769a9761
SuriChen1028 <shirui.chan@gmail.com>
2022-07-13 10:43:39 -0500

    add simulation code

---
+++ src/simulation.py
@@ -0,0 +1,207 @@
+import numpy as np
+import pandas as pd
+import pickle
+import SolveLinSys
+from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import CubicSpline
+
+def Damage_Intensity(Yt, y_bar_lower=1.5):
+    r_1 = 1.5
+    r_2 = 2.5
+    Intensity = r_1 * (np.exp(r_2 / 2 * (Yt - y_bar_lower)**2) -1) * (Yt > y_bar_lower)
+    return Intensity
+
+
+def simulate_pre(
+    grid = (), model_args = (), controls = (), initial=(np.log(85/0.115), 1.1, -3.7), 
+    T0=0, T=40, dt=1/12
+                ):
+
+    K, Y, L = grid
+    K_min, K_max, Y_min, Y_max, L_min, L_max = min(K), max(K), min(Y), max(Y), min(L), max(L)
+    hK, hY = K[1] - K[0], Y[1] - Y[0]
+    (K_mat, Y_mat, L_mat) = np.meshgrid(K, Y, L, indexing = 'ij')
+
+    delta, alpha, mu_k, kappa, sigma_k, beta_f, zeta, psi_0, psi_1, sigma_g, theta, lambda_bar, vartheta_bar, = model_args
+    ii, ee, xx, g_tech, g_damage, pi_c = controls
+    
+    n_climate = len(pi_c)
+
+    method = 'linear'
+    years  = np.arange(T0, T0 + T + dt, dt)
+    pers   = len(years)
+       
+
+    # setting up grids
+    stateSpace = np.hstack([
+        K_mat.reshape(-1,1,order = "F"), 
+        Y_mat.reshape(-1,1,order = "F"),
+        L_mat.reshape(-1,1,order = "F"),
+    ])
+
+    # some parameters remaiend unchanged across runs
+    gamma_1  = 0.00017675
+    gamma_2  = 2. * 0.0022
+    beta_f   = 1.86 / 1000
+    sigma_y  = 1.2 * 1.86 / 1000
+    
+    theta_ell = pd.read_csv("./model144.csv", header=None).to_numpy()[:, 0]/1000.
+    pi_c_o = np.ones(len(theta_ell)) / len(theta_ell)
+    pi_c_o = np.array([temp * np.ones(K_mat.shape) for temp in pi_c_o])
+    theta_ell = np.array([temp * np.ones(K_mat.shape) for temp in theta_ell])
+    args = (delta, alpha, kappa, mu_k, sigma_k, gamma_1, gamma_2, theta_ell, pi_c_o, sigma_y,  theta, vartheta_bar, lambda_bar)
+
+#     v, ME_base, diff = decompose(v0, stateSpace, (K_mat, Y_mat, L_mat), (ii, ee, xx), args=args)
+
+    gridpoints = (K, Y, L)
+    i_func = RegularGridInterpolator(gridpoints, ii)
+    e_func = RegularGridInterpolator(gridpoints, ee)
+    x_func = RegularGridInterpolator(gridpoints, xx)
+    tech_func = RegularGridInterpolator(gridpoints, g_tech)
+#     ME_base_func = RegularGridInterpolator(gridpoints, ME_base)
+
+#     if pre_damage:
+    n_damage = len(g_damage)
+
+    damage_func_list = []
+    for i in range(n_damage):
+        func_i = RegularGridInterpolator(gridpoints, g_damage[i])
+        damage_func_list.append(func_i)
+        
+    climate_func_list = []
+    for i in range(n_climate):
+        func_i = RegularGridInterpolator(gridpoints, pi_c[i])
+        climate_func_list.append(func_i)
+
+
+    def get_i(x):
+        return i_func(x)
+
+    def get_e(x):
+        return e_func(x)
+    
+    def get_x(x):
+        return x_func(x)
+
+
+#     K_0 = np.log(85 / 0.115)
+#     Y_0 = 1.1
+#     L_0 = -3.7
+
+    K_0, Y_0, L_0 = initial
+
+    def mu_K(i_x):
+        return mu_k + i_x - 0.5 * kappa * i_x ** 2  - 0.5 * sigma_k ** 2
+    
+    def mu_L(Xt, state):
+        return -zeta + psi_0 * (Xt * (np.exp(state[0] - state[2]) ) )**psi_1 - 0.5 * sigma_g**2
+    
+    
+    hist      = np.zeros([pers, 3])
+    i_hist    = np.zeros([pers])
+    e_hist    = np.zeros([pers])
+    x_hist    = np.zeros([pers])
+    scc_hist  = np.zeros([pers])
+    gt_tech   = np.zeros([pers])
+#     if pre_damage:
+    gt_dmg    = np.zeros([n_damage, pers])
+    pi_c_t = np.zeros([n_climate, pers])
+
+#     ME_base_t = np.zeros([pers])
+
+    mu_K_hist = np.zeros([pers])
+    mu_L_hist = np.zeros([pers])
+
+    for tm in range(pers):
+        if tm == 0:
+            # initial points
+            hist[0,:] = [K_0, Y_0, L_0] # logL
+            i_hist[0] = get_i(hist[0, :])
+            e_hist[0] = get_e(hist[0, :])
+            x_hist[0] = get_x(hist[0, :])
+            mu_K_hist[0] = mu_K(i_hist[0])
+            mu_L_hist[0] = mu_L(x_hist[0], hist[0,:])
+            gt_tech[0] = tech_func(hist[0, :])
+#             if pre_damage:
+            for i in range(n_damage):
+                damage_func = damage_func_list[i]
+                gt_dmg[i, 0] = damage_func(hist[0, :])
+            
+            for i in range(n_climate):
+                climate_func = climate_func_list[i]
+                pi_c_t[i, 0] = climate_func(hist[0, :])
+
+#             gt_dmg[0] = damage_func(hist[0, :])
+#             ME_base_t[0] = ME_base_func(hist[0, :])
+
+#             scc_hist[0] = scc_func(hist[0, :])
+
+        else:
+            # other periods
+            i_hist[tm] = get_i(hist[tm-1,:])
+            e_hist[tm] = get_e(hist[tm-1,:])
+            x_hist[tm] = get_x(hist[tm-1,:])
+            gt_tech[tm] = tech_func(hist[tm-1,:])
+#             if pre_damage:
+            for i in range(n_damage):
+                damage_func = damage_func_list[i]
+                gt_dmg[i, tm] = damage_func(hist[tm-1, :])
+
+            for i in range(n_climate):
+                climate_func = climate_func_list[i]
+                pi_c_t[i, tm] = climate_func(hist[tm -1, :])
+
+#             ME_base_t[tm] = ME_base_func(hist[tm-1, :])
+
+
+            mu_K_hist[tm] = mu_K(i_hist[tm])
+            mu_L_hist[tm] = mu_L(x_hist[tm], hist[tm-1, :])
+
+            hist[tm,0] = hist[tm-1,0] + mu_K_hist[tm] * dt #logK
+            hist[tm,1] = hist[tm-1,1] + beta_f * e_hist[tm] * dt
+            hist[tm,2] = hist[tm-1,2] + mu_L_hist[tm] * dt # logλ
+        
+    
+    
+        # using Kt instead of K0
+    jt = 1 - e_hist/ (alpha * lambda_bar * np.exp(hist[:, 0]))
+    jt[jt <= 1e-16] = 1e-16
+    LHS = theta * vartheta_bar / lambda_bar * jt**(theta -1)
+    MC = delta / (alpha  - i_hist - alpha * vartheta_bar * jt**theta - x_hist)
+
+    
+    scc_hist = LHS * 1000
+#     scc_0 = ME_base_t / MC * 1000 * np.exp(hist[:, 0])
+    
+    distorted_tech_intensity = np.exp(hist[:, 2]) * gt_tech
+    distorted_tech_prob = 1 - np.exp(- np.cumsum(np.insert(distorted_tech_intensity * dt, 0, 0) ))[:-1]
+
+#     if pre_damage:
+    damage_intensity = Damage_Intensity(hist[:, 1])
+    distorted_damage_intensity = np.mean(gt_dmg, axis=0) * damage_intensity
+    distorted_damage_prob = 1 - np.exp(- np.cumsum(np.insert(distorted_damage_intensity * dt, 0, 0) ))[:-1]
+
+    
+    res = dict(
+        states= hist, 
+        i = i_hist * np.exp(hist[:, 0]), 
+        e = e_hist,
+        X = x_hist * np.exp(hist[:, 0]), 
+        x = x_hist / alpha,
+        scc = scc_hist,
+#         scc0 = scc_0,
+        gt_tech = gt_tech,
+        gt_dmg = gt_dmg,
+        distorted_damage_prob=distorted_damage_prob,
+        distorted_tech_prob=distorted_tech_prob,
+        pic_t = pi_c_t,
+#         ME_base = ME_base_t,
+        jt = jt,
+        LHS = LHS,
+        years=years
+    )
+
+#     if pre_damage:
+#         res["gt_dmg"] = gt_dmg
+
+    return res
